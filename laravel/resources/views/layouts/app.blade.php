08fb56775c993dd68c09a2ad454959bdf76ef347
Timothy Batt <jammincoderguy96@gmail.com>
2022-11-07 10:54:09 -0500

    link `app.css` after everything else

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css'); }}">
+    
     <link rel="stylesheet" href="{{ URL::asset('css/tailwind.css'); }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css'); }}">
     <title>@yield('title') - Tim's Blog</title>
 </head>
 <body>
