0eb612b934504a033b27e3da8fead18f475c0965
Timothy Batt <jammincoderguy96@gmail.com>
2022-11-07 12:37:09 -0500

    tweaked delete

--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -9,17 +9,19 @@
 @section("content")
 <section class="post-content-card">
     @if (Auth::check() && Auth::user()->user_id === $post->author_id)
-        <div class="modify-post-header flex gap=1em justify=space-between">
-            <a href="{{ route('post.edit', [$post->post_id]); }}" class="">Edit</a>
-            <button id="delete_post_button" class="btn-danger" onclick="confirmDelete();">Delete</button>
-            
-            <form id="delete_post" class="hide" method="POST" action="{{ route('post.delete', [$post->post_id]) }}">
+        <div class="modify-post-header">
+            <div class="flex gap-4">
+                <a href="{{ route('post.edit', [$post->post_id]); }}" class="">Edit</a>
+                <button id="delete_post_button" class="btn-danger" onclick="confirmDelete();">Delete</button>
+            </div>
+
+            <form id="delete_post" class="hidden" method="POST" action="{{ route('post.delete', [$post->post_id]) }}">
                 @csrf
+                <div>Are you sure you want to delete this post?</div>
                 
                 <a onclick="event.preventDefault(); this.closest('form').submit();" 
                 href="{{ route('post.delete', [$post->post_id]); }}" 
                 class="color=red">DELETE</a>
-                <div>Are you sure you want to delete this post?</div>
             </form>
         </div>
     @endif
--- public_html/js/app.js
+++ public_html/js/app.js
@@ -40,10 +40,11 @@ function confirmDelete()
   };
   let deletePostForm = document.getElementById('delete_post');
   let deletePostButton = document.getElementById('delete_post_button');
-  deletePostForm.classList.toggle('hide');
+  deletePostForm.classList.toggle('hidden');
 
   if (deletePostButton.innerText === text.delete) {
     deletePostButton.innerText = text.cancel;
+
   } else {
     deletePostButton.innerText = text.delete;
   }
