444171240b25fd37d8d70239d542d5bf730abd3b
Timothy Batt <jammincoderguy96@gmail.com>
2022-11-11 20:40:11 -0500

    working on forms

--- laravel/resources/views/create-post.blade.php
+++ laravel/resources/views/create-post.blade.php
@@ -4,17 +4,18 @@
 
 @section("content")
 
-<div class="flex flex-col items-center">
-<form id="markdown_form" action="{{ route('post.store') }}" method="POST" enctype="multipart/form-data" class="flex flex-col align=center">
+<div class="flex flex-col items-center px-1 mt-4">
+    <form class="flex flex-col align-center" id="markdown_form" action="{{ route('post.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <input type="hidden" value="{{ $post->post_id }}" name="post_id"/>
         <div class="flex items-center flex-col gap-4 mb-12">
             <label for="post_title">Post Title</label>    
-            <input type="text" name="post_title" id="post_title" class="text-black one-line-input font-bold text-2xl text-center" placeholder="Title">
+            <input type="text" name="post_title" id="post_title" class="font-bold text-2xl text-center" placeholder="Title">
             
             <br>
+
             <label for="post_description">Post Description</label>
-            <input type="text" name="post_description" id="post_description" class="text-black one-line-input font-description text-center text-sm max-w-[60ch]" placeholder="Describe your post breifly">
+            <input type="text" name="post_description" id="post_description" class="font-description text-center text-sm max-w-[60ch]" placeholder="Describe your post breifly">
         </div>
 
         <textarea name="post_content" id="post_content"></textarea>
--- laravel/resources/views/edit-post.blade.php
+++ laravel/resources/views/edit-post.blade.php
@@ -3,29 +3,33 @@
 @section('title', "Edit Post")
 @section('content')
 
-<div class="flex flex-col align=center px=1 mt=4">
-    <section class="card shadow mx=0.2em mb=2em post-content-card">
+<div class="flex flex-col align-center px-1 mt-4">
+    <section class="card shadow mx-1 mb-8 post-content-card">
         <form id="markdown_form" action="{{ route('post.update'); }}" method="POST" enctype="multipart/form-data">
             @csrf
-            <div class="flex align=center flex-col gap=1em mb=3em p=1em">
-                <h1 class="text=center d=flex flex-col">
+            <div class="flex align-center flex-col gap-4 mb-[3em] p-[1em]">
+                <h1 class="text-center flex flex-col">
                     <label for="post_title">Title</label><br>
-                    <input type="text" id="post_title" name="post_title" value="{{ $post->post_title }}">
-                    
+                    <input class="text-black font-bold text-2xl text-center" type="text" id="post_title" name="post_title" value="{{ $post->post_title }}">
                 </h1>
                 
-                <small class="post-description text=center">
+                <small class="post-description text-center">
                     <label for="post_description">Description</label><br>
-                    <input type="text" id="post_description" name="post_description" value="{{ $post->post_description }}">
+                    <input class="text-black font-bold text-2xl text-center" type="text" id="post_description" name="post_description" value="{{ $post->post_description }}">
                 </small>
 
                 
             </div>
+
             <input type="hidden" id='post_id' name="post_id" value="{{ $post->post_id }}">
-            <textarea ondrop="dropHandler(event);"  class="width=100% p=1" name="content" id="content">{{ $post->content }}</textarea>
+            
+            <textarea ondrop="dropHandler(event);"  class="w-[100%] p-4" name="content" id="content">{{ $post->content }}</textarea>
+            
             <button>Save</button>
         </form>
+
         <a href="{{ route('post.display', [$post->post_id]) }}">Cancel</a>
+        
         <input type="file" id="upload_image" name="upload_image">
 
     </section>
