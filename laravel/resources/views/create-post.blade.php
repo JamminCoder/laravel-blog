923c2a4e5b6231d6444ba878b292e4828dc8a423
Timothy Batt <jammincoderguy96@gmail.com>
2022-11-14 14:30:46 -0500

    improved post creation layout

--- laravel/resources/views/create-post.blade.php
+++ laravel/resources/views/create-post.blade.php
@@ -4,23 +4,22 @@
 
 @section("content")
 
-<div class="flex flex-col items-center px-1 mt-4">
-    <form class="flex flex-col align-center" id="markdown_form" action="{{ route('post.store') }}" method="POST" enctype="multipart/form-data">
+<div class="grid place-content-center px-1 mt-4">
+
+    <form class="max-w-[30rem] w-[100%]" id="markdown_form" action="{{ route('post.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <input type="hidden" value="{{ $post->post_id }}" name="post_id"/>
-        <div class="flex items-center flex-col gap-4 mb-12">
-            <label for="post_title">Post Title</label>    
-            <input type="text" name="post_title" id="post_title" class="font-bold text-2xl text-center" placeholder="Title">
+        <div class="flex flex-col gap-4 mb-12">  
+            <input type="text" name="post_title" id="post_title" class="bg-[#1e1e1e] font-bold text-2xl border-x-0 border-t-0 border-b-slate-700" placeholder="Title">
             
             <br>
 
-            <label for="post_description">Post Description</label>
-            <input type="text" name="post_description" id="post_description" class="font-description text-center text-sm max-w-[60ch]" placeholder="Describe your post breifly">
+            <input type="text" name="post_description" id="post_description" class="bg-[#1e1e1e] font-bold text-2xl border-x-0 border-t-0 border-b-slate-700" placeholder="Describe your post breifly">
         </div>
 
-        <textarea name="post_content" id="post_content"></textarea>
+        <textarea class="h-[60vh] w-[100%]" name="post_content" id="post_content"></textarea>
 
-        <button class="p-2 bg-green-500 rounded">Submit</button>
+        <button class="p-2 bg-green-500 rounded block">Submit</button>
     </form>
 
     <script src="{{ URL::asset('js/handle_file_upload.js'); }}"></script>
--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,37 +1,3 @@
-// Auto resizing textarea code from: https://codepen.io/shshaw/pen/azzvBv
-
-(function(){
-  
-  var textareas = document.querySelectorAll('textarea'),
-      
-      resize = function(t) {
-        t.style.height = 'auto';
-        t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
-        t.style.height = (t.scrollHeight + t.offset ) + 'px';
-        t.style.overflow = '';
-      },
-      
-      attachResize = function(t) {
-        if ( t ) {
-          t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
-
-          resize(t);
-
-          if ( t.addEventListener ) {
-            t.addEventListener('input', function() { resize(t); });
-            t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
-          }
-
-          t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
-        }
-      };
-  
-  for (var i = 0; i < textareas.length; i++ ) {
-    attachResize(textareas[i]);
-  }
-  
-})();
-
 function confirmDelete()
 {
   let text = {
