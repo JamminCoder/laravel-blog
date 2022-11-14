9751e1d880230e3c095f50e4262a5bea313f41ff
Timothy Batt <jammincoderguy96@gmail.com>
2022-11-14 14:34:25 -0500

    improved post edit layout

--- laravel/resources/views/edit-post.blade.php
+++ laravel/resources/views/edit-post.blade.php
@@ -3,36 +3,29 @@
 @section('title', "Edit Post")
 @section('content')
 
-<div class="flex flex-col align-center px-1 mt-4">
-    <section class="card shadow mx-1 mb-8 post-content-card">
-        <form id="markdown_form" action="{{ route('post.update'); }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="flex align-center flex-col gap-4 mb-[3em] p-[1em]">
-                <h1 class="text-center flex flex-col">
-                    <label for="post_title">Title</label><br>
-                    <input class="text-black font-bold text-2xl text-center" type="text" id="post_title" name="post_title" value="{{ $post->post_title }}">
-                </h1>
-                
-                <small class="post-description text-center">
-                    <label for="post_description">Description</label><br>
-                    <input class="text-black font-bold text-2xl text-center" type="text" id="post_description" name="post_description" value="{{ $post->post_description }}">
-                </small>
+<div class="grid place-content-center px-1 mt-4">
+    <form class="max-w-[30rem] w-[100%]" id="markdown_form" action="{{ route('post.update'); }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="flex align-center flex-col gap-4 mb-[3em] p-[1em]">
+            <input class="bg-[#1e1e1e] font-bold text-2xl border-x-0 border-t-0 border-b-slate-700" type="text" id="post_title" name="post_title" value="{{ $post->post_title }}">
 
-                
-            </div>
+            <input class="bg-[#1e1e1e] font-bold text-2xl border-x-0 border-t-0 border-b-slate-700" type="text" id="post_description" name="post_description" value="{{ $post->post_description }}">
 
-            <input type="hidden" id='post_id' name="post_id" value="{{ $post->post_id }}">
-            
-            <textarea ondrop="dropHandler(event);"  class="w-[100%] p-4" name="content" id="content">{{ $post->content }}</textarea>
-            
-            <button>Save</button>
-        </form>
+        </div>
 
-        <a href="{{ route('post.display', [$post->post_id]) }}">Cancel</a>
+        <input type="hidden" id='post_id' name="post_id" value="{{ $post->post_id }}">
         
-        <input type="file" id="upload_image" name="upload_image">
+        <textarea ondrop="dropHandler(event);"  class="h-[60vh] w-[100%] p-4" name="content" id="content">{{ $post->content }}</textarea>
+        
+        <button class="p-2 bg-green-500 rounded block mb-4">Save</button>
+    </form>
+    
+    <input class="mb-4" type="file" id="upload_image" name="upload_image">
 
-    </section>
+    <a href="{{ route('post.display', [$post->post_id]) }}">Cancel</a>
+    
+    
+
     <script src="{{ URL::asset('js/handle_file_upload.js'); }}"></script>
     <script src="{{ URL::asset('js/app.js'); }}"></script>
 </div>
